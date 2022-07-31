52118aeeddaa0941cf57dedf9d67d989c88f23f0
Declan <syas0301@gmail.com>
2022-07-31 13:02:22 +0900

    Containers are framed.

--- server/server.js
+++ server/server.js
@@ -1,15 +1,44 @@
+const { Server } = require("socket.io");
 const express = require("express");
 const app = express();
+const compression = require("compression");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const { resolve } = require("path");
 
-const port = process.env.PORT || 5000;
+const Logger = require("./Logger");
+const log = new Logger("server");
 
-const server = require("http").createServer(app);
+const route = require("./routes/router.js");
 
-app.use(cors());
+let io, server, port;
+
+port = process.env.PORT || 3000;
+
+server = require("http").createServer(app);
+io = new Server({
+    maxHttpBufferSize: 1e7,
+    transports: ["websocket"],
+}).listen(server);
+
+// directory
+const dir = {
+    public: resolve(__dirname, "../public"),
+};
+
+let channels = {};
+let sockets = {};
+let peers = {};
+
+app.set("view engine", "ejs");
+
 app.use(route);
+app.use(cors());
+app.use(compression());
 app.use(express.json());
+app.use(express.static(dir.public));
+app.use(bodyParser.urlencoded({ extended: true })); // Need for Slack API body parser
 
 server.listen(port, () => {
-    console.debug(`Server Listen: ${port}`);
+    log.debug(`%c SIGN-SERVER started...`);
 });
