4efb682872aa524fb652eef8106e9622d117c683
Declan <syas0301@gmail.com>
2022-08-07 00:32:33 +0900

    User Getter Setter 설정

--- public/js/model/User/User.js
+++ public/js/model/User/User.js
@@ -1,2 +1,28 @@
 export class User {
+    constructor(name, channel) {
+        // SessionStorage를 통한 Set,Get 처리를 하는 곳
+
+        this._name = name;
+        this._channel = channel;
+    }
+
+    get name() {
+        return this._name.toUpperCase();
+    }
+
+    set name(userId) {
+        if (userId) {
+            this._name = userId;
+        }
+    }
+
+    get channel() {
+        return this._channel.toUpperCase();
+    }
+
+    set channel(channel) {
+        if (channel) {
+            this._channel = channel;
+        }
+    }
 }
